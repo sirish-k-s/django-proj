6b90b0d25121c2cb3cef69bbcff368a7bc6ed398
Sirish KS <siks@synamedia.com>
2023-05-26 22:42:22 +0530

    for vercel

--- Star_Social/Star_Social/settings.py
+++ Star_Social/Star_Social/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=d&ad1e9e8l%x_l*!w$5-jbq-+=jcay$%jtn=%fb7v-o@tfh^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles_build'/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
