23551a87f9db7ef6d7c58894c4c95df4887903b2
Sirish KS <siks@synamedia.com>
2023-05-26 22:46:33 +0530

    for build.sh

--- Star_Social/build_files.sh
+++ Star_Social/build_files.sh
@@ -1,6 +1,23 @@
-echo "BUILD START"
+#!/bin/bash
 
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatics --noinput --clear
+# Activate the virtual environment (if you are using one)
+# Replace "path_to_venv" with the actual path to your virtual environment
+# source path_to_venv/bin/activate
 
-echo "BUILD END"
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
+
+# Restart the web server (if necessary)
+# Replace "web_server_command" with the command to restart your web server (e.g., "sudo systemctl restart nginx")
+web_server_command
+
+# Optionally, run any other setup or build steps required for your application
+
+# Deactivate the virtual environment (if you are using one)
+#deactivate
